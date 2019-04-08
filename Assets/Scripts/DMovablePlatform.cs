772a4102faddc78c08a1eebc3021c1538d7f094d
DedSec94 <matteocosimi94@gmail.com>
2019-04-08 13:49:43 +0200

    Added Moving Platform with Door

---
+++ Assets/Scripts/DMovablePlatform.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DMovablePlatform : MonoBehaviour
+{
+    public GameObject opponent;
+    public GameObject opponentToMove;
+    public float distance;
+    Transform camTransform;
+    public float distbetween;
+
+    void Start()
+    {
+        camTransform = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+        distance = camTransform.position.y - transform.position.y;
+        opponentToMove = null;
+    }
+
+    void Update()
+    {
+        //if (opponentToMove == opponent)
+        //{
+        //    if (opponent.gameObject.tag == "PDW1")
+        //    {
+        //        opponentToMove.transform.position = new Vector3(transform.position.x + distbetween, opponentToMove.transform.position.y, opponentToMove.transform.position.z);
+        //    }
+        //    else if (opponent.gameObject.tag == "PDW2")
+        //    {
+        //        opponentToMove.transform.position = new Vector3(transform.position.x - distbetween, opponentToMove.transform.position.y, opponentToMove.transform.position.z);
+        //    }
+        //}
+
+        if (opponentToMove == opponent)
+        {
+            opponentToMove.transform.position = new Vector3(-transform.position.x, opponentToMove.transform.position.y, opponentToMove.transform.position.z);
+        }
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = objPosition;
+    }
+    void OnMouseDown()
+    {
+        opponentToMove = opponent;
+    }
+    void OnMouseUp()
+    {
+        opponentToMove = null;
+    }
+}
