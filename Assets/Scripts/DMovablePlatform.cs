fd2486aed59b754653bbec15e040b910433ecc6f
DedSec94 <matteocosimi94@gmail.com>
2019-04-09 11:21:07 +0200

    Fixed Problems with Double Movable Platform

--- Assets/Scripts/DMovablePlatform.cs
+++ Assets/Scripts/DMovablePlatform.cs
@@ -2,18 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(MovablePlatform))]
 public class DMovablePlatform : MonoBehaviour
 {
     public GameObject opponent;
     public GameObject opponentToMove;
-    public float distance;
-    Transform camTransform;
-    public float distbetween;
 
     void Start()
     {
-        camTransform = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
-        distance = camTransform.position.y - transform.position.y;
         opponentToMove = null;
     }
 
@@ -23,13 +19,6 @@ public class DMovablePlatform : MonoBehaviour
         {
             opponentToMove.transform.position = new Vector3(-transform.position.x, opponentToMove.transform.position.y, opponentToMove.transform.position.z);
         }
-    }
-
-    void OnMouseDrag()
-    {
-        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
-        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        transform.position = objPosition;
     }
     void OnMouseDown()
     {
