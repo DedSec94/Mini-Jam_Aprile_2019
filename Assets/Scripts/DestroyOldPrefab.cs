636bb5b5157a51e09426158eb01279ecadcdc62d
DedSec94 <matteocosimi94@gmail.com>
2019-04-10 20:17:33 +0200

    FakeProceduralLevelTest

---
+++ Assets/Scripts/DestroyOldPrefab.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOldPrefab : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject.GetComponent<Transform>().parent.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region PUBLIC
+    public GameObject[] prefabs;
+    #endregion
+
+    #region PRIVATE
+
+    #endregion
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
