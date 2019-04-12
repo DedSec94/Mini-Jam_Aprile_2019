0c2307b4560688af00d1a9ab8a6b900ae1825671
Joshua <nigellus@hotmail.it>
2019-04-12 17:51:54 +0200

    Alternate Blocking Walls
    Added Alternate Blocking Walls

---
+++ Assets/Scripts/AlternateWall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlternateWall : MonoBehaviour
+{
+    public GameObject Altro;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        Altro.SetActive(true);
+        //Altro.GetComponent<Renderer>().material.color = Color.yellow;
+
+        gameObject.SetActive(false);
+        //gameObject.GetComponent<Renderer>().material.color = Color.red;
+    }
+}
