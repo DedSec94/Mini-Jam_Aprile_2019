df94078ecc224c6a503a1c263ca851e647d87c63
DedSec94 <matteocosimi94@gmail.com>
2019-04-16 18:38:57 +0200

    Delete Some Folders

--- Assets/Scripts/InputSO.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "InputSO",menuName ="Scriptables/InputSO")]
-public class InputSO : ScriptableObject
-{
-    public KeyCode jumpKey;
-}
